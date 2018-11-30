c199072f9340ed99b987228430e0526d9816c252
Roy Baxter <delarch@verizon.net>
2018-11-30 12:56:25 -0500

    Assets and instructions for Gulp integration

---
+++ gulp-assets/gulpfile.js
@@ -0,0 +1,35 @@
+var theme = 'bones';
+var gulp = require('gulp');
+var compass = require('gulp-compass');
+var minify = require('gulp-uglify');
+var jssources = 'wp-content/themes/'+theme+'/library/jssrc/**/*.js';
+var jsdest = 'wp-content/themes/'+theme+'/library/js';
+var csssource = 'wp-content/themes/'+theme+'/library/scss/**/*.scss';
+var cssdest = 'wp-content/themes/'+theme+'/library/css';
+
+gulp.task('default', ['compress']);
+
+function error(err) {
+  console.log(err);
+  this.emit('end');
+}
+
+gulp.task('compass', function() {
+  gulp.src(csssource)
+  .pipe(compass({
+    config_file: 'config.rb',
+    css: cssdest,
+    sass: 'wp-content/themes/'+theme+'/library/scss'
+  }).on('error', error));
+});
+
+gulp.task('watch', ['compass', 'compress'], function(){
+  gulp.watch(jssources, ['compress']);
+  gulp.watch(csssource, ['compass']);
+});
+
+gulp.task('compress', function(){
+  return gulp.src(jssources)
+  .pipe(minify()).on('error', error)
+  .pipe(gulp.dest(jsdest))
+});
---
+++ gulp-assets/readme.txt
@@ -0,0 +1,29 @@
+######### To use Gulp for SASS and JS minifaction ##########
+
+Requires node.js and compass
+
+
+Copy the following files to the root of your Wordpress installation:
+config.rb
+gulpfile.js
+package.json
+package-lock.json
+
+If using GIT, create a .gitignore file in the root of your 
+installation and add node_modules to it.
+
+Do similar for other version control systems you may use.
+
+Edit config.rb, changing "bones" to the name of your theme
+in all directory definitions under #2.
+
+Edit the first line of gulpfile.js to similarly change
+"bones" to the name of your theme.
+
+Finally, from a command line window on the site root, 
+run "npm install" to install all necessary modules.
+
+To use: from command line, type "gulp watch" and enter.
+This will begin watching for changes in your SASS and 
+javascript files. Make sass changes in library/scss
+and javascript changes in library/jssrc only.
